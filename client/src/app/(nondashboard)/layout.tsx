888d0da330325cfb54935cd8059ccc8c61d37767
aditya <adityakulkarni54321@gmail.com>
2025-06-13 01:07:57 +0530

    changes

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -29,7 +29,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
 
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full bg-white">
       <Navbar />
 
       <main
